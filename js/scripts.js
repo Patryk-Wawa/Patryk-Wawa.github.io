435578c8deb25ed44c40502a0002448d0129dbe2
Patryk-Wawa <nadawcamaila@o2.pl>
2017-08-27 18:19:13 +0200

    quote v.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+var prefix = "https://cors-anywhere.herokuapp.com/";
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 function getQuote() {
@@ -21,11 +22,12 @@ function createTweet(input) {
 		$('.author').text("Author: " + quoteAuthor);
 		$('.tweet').attr('href', tweet);
 	}
-	$(document).ready(function() {
-		getQuote();
-		$('.trigger').click(function() {
-			getQuote();
-		})
-	});
 }
-
+$(document).ready(function() {
+	getQuote();
+	$('.trigger').click(function() {
+		getQuote();
+	})
+	$.getJSON(prefix + quoteUrl, createTweet);
+	$.ajaxSetup({ cache: false });
+});
